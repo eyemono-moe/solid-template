117c30289897897719f4c93cbd8a4bac0fc5ec7c
eyemono-moe <ysdr83@gmail.com>
2024-08-09 21:31:37 +0900

    add biome

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 /* @refresh reload */
-import { render } from 'solid-js/web'
+import { render } from "solid-js/web";
 
-import './index.css'
-import App from './App'
+import App from "./App";
 
-const root = document.getElementById('root')
+// biome-ignore lint/style/noNonNullAssertion: div#root in index.html
+const root = document.getElementById("root")!;
 
-render(() => <App />, root!)
+render(() => <App />, root);
