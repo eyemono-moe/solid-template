1c74eb1d4412882cbc8c4ec0d82b4df5008d152a
eyemono-moe <ysdr83@gmail.com>
2024-08-09 21:29:22 +0900

    remove default content

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { Component } from 'solid-js';
+import type { Component } from "solid-js";
 
 const App: Component = () => {
   return (
     <div>
       <h2>App</h2>
     </div>
-  )
-}
+  );
+};
 
 export default App;
